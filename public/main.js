10dcadba9cb60f96b1b0d1080cdf78b705c3a1ce
Alvin Ng <ngalvin93@gmail.com>
2019-09-22 23:53:24 -0500

    clean up css

--- public/main.js
+++ public/main.js
@@ -1,20 +1,19 @@
-let todoList = document.querySelector('#todos')
+const todoList = document.querySelector('#todos-child')
 
 document.addEventListener('DOMContentLoaded', function fetchTodoList () {
     $.getJSON('/api/todos')
-        .then(function(response){
-            return response
-        })
+        .then(function(response){return response})
         .then(renderTodoList)
 })
 
 function renderTodoList (response) {
-    console.log('%cThis is the todo list:', "color: red;")
-    response.forEach(function(singleItem){
-        console.log(singleItem.todo)
-    })
     const todoHtml = response.map(function(singleItem){
         return `<p id="todoItem">${singleItem.todo}</p>`
     })
     todoList.innerHTML = todoHtml.join('')
 }
+
+const addButton = document.querySelector('#add-button')
+addButton.addEventListener('click', function (e) {
+    console.log(addButton)
+})
