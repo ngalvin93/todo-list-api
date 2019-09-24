7f8e4d2a1e7ff856719838588b75100430c3e678
Alvin Ng <ngalvin93@gmail.com>
2019-09-23 22:57:35 -0500

    progress

--- public/main.js
+++ public/main.js
@@ -4,16 +4,47 @@ document.addEventListener('DOMContentLoaded', function fetchTodoList () {
     $.getJSON('/api/todos')
         .then(function(response){return response})
         .then(renderTodoList)
+    // document.addEventListener('click', function (e) {
+    //     console.log(e.target)
+    // })
 })
 
 function renderTodoList (response) {
     const todoHtml = response.map(function(singleItem){
-        return `<p id="todoItem">${singleItem.todo}</p>`
+        return `<p class="todo-item">${singleItem.todo}</p>`
     })
     todoList.innerHTML = todoHtml.join('')
 }
 
-const addButton = document.querySelector('#add-button')
-addButton.addEventListener('click', function (e) {
-    console.log(addButton)
+// const addBtn = document.getElementById('add-button')
+// const addingItem = document.getElementById('add-item').value
+
+// addBtn.addEventListener('click', function (e) {
+//     e.preventDefault()
+//     console.log('You clicked the submit button')
+//     console.log(addingItem)
+// })
+
+const btn = document.getElementById("btn")
+
+btn.addEventListener('click', function () {
+    const todoString = document.getElementById("input-txt").value
+    const todoPromise = postTodoItem('/api/todos', {todo: todoString})
+    todoPromise
+        .then(response => renderTodoList(response))
+        .catch(error => console.log(error))
 })
+
+function postTodoItem (url, todoItem) {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todoItem)
+    })
+    .then(function (response) {
+        const result = response.json()
+        console.log(result)
+    })
+}
